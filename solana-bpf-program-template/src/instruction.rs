2863fe68d085c35f3fad795bc879a3f488ceb563
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-10-14 03:50:11 -0400

    completed till state.rs

--- solana-bpf-program-template/src/instruction.rs
+++ solana-bpf-program-template/src/instruction.rs
@@ -1,5 +1,5 @@
-use std::convert::TryInto;
 use solana_program::program_error::ProgramError;
+use std::convert::TryInto;
 
 use crate::error::EscrowError::InvalidInstruction;
 pub enum EscrowInstruction {
@@ -13,23 +13,23 @@ pub enum EscrowInstruction {
     /// [] the token program
     InitEscrow {
         /// the amourt party A expects to receive of token Y
-        amount: u64
-    }
+        amount: u64,
+    },
 }
 
-impl EscrowInstruction{
+impl EscrowInstruction {
     pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
-        let (tag,rest) = input.split_first().ok_or(InvalidInstruction)?;
+        let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
 
         Ok(match tag {
-            0 => Self::InitEscrow{
-                amount:Self::unpack_amount(rest)?
+            0 => Self::InitEscrow {
+                amount: Self::unpack_amount(rest)?,
             },
-            _=> return Err(InvalidInstruction.into()),
+            _ => return Err(InvalidInstruction.into()),
         })
     }
 
-    fn unpack_amount(input:&[u8]) -> Result<u64, ProgramError>{
+    fn unpack_amount(input: &[u8]) -> Result<u64, ProgramError> {
         let amount = input
             .get(..8)
             .and_then(|slice| slice.try_into().ok())
